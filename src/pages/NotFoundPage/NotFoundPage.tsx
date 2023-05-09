4ba3fca7404a4cff5316c31536b5b9e9d1c94716
Kateryna Babina <katerynababina01@gmail.com>
2023-05-09 09:48:15 +0300

    created Not found page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,3 +1,19 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
+import './NotFoundPage.scss';
 
-export const NotFoundPage: FC = () => (<h1>Page Not Found</h1>);
+export const NotFoundPage: FC = () => (
+  <div className="not-found-page">
+    <h1 className="not-found-page__heading">
+      404
+    </h1>
+
+    <p className="not-found-page__message">
+      Oops! Page not found!
+    </p>
+
+    <Link to="/" className="not-found-page__link">
+      Go To Homepage
+    </Link>
+  </div>
+);
