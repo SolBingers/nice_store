085fc84c192e9745e3d2e2b0519afc0c5a5130bf
Bogdan <bodia-00wot@ukr.net>
2023-05-09 17:40:36 +0300

    added styles and adaptive

--- src/components/SaleBanner/SaleBanner.tsx
+++ src/components/SaleBanner/SaleBanner.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import saleBanner from './SaleBanner.module.scss';
+import { Button } from '../Button';
+import rock from '../../images/rock.svg';
 
 export const SaleBanner: React.FC = () => {
 
   return (
-    <div className= { saleBanner.saleBanner}>
-      
+    <div className= {saleBanner.saleBanner}>
+      <div className={saleBanner.bannerBlack}>
+        <h3 className={saleBanner.big}>
+          BIG
+        </h3>
+
+        <h2 className={saleBanner.sale}>
+          SALE
+        </h2>
+
+        <div className={saleBanner.button}>
+          <Button 
+            text='Shop Now' 
+            type='secondary' 
+            size='large'
+          />
+        </div>
+      </div>
+
+      <div className={saleBanner.bannerWhite}>
+        <img 
+          src={rock} 
+          className={saleBanner.rock} 
+          alt='rock'
+        />
+  
+        <div className={saleBanner.saleInfo}>
+          save up to <span className={saleBanner.yellowBold}>50%</span> off
+        </div>
+      </div>
     </div>
   );
 };
