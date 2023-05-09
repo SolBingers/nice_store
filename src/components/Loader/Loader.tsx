a366c57eb91039f1f4019e5b7c2d901919fc9844
Bogdan <bodia-00wot@ukr.net>
2023-05-09 13:27:15 +0300

    solution

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import './loader.scss';
-// es-lint disable-next-line
-import dog from '../../images/dog.svg';
+import MyLogo from '../../images/dog.svg';
 
 export const Loader: React.FC = () => (
   <div className='container'>
     <div className="loader">
       <div className="loader_circle">
-        <img src={ dog } alt="loader_dog" className='loader_dog'></img>
+        <img src={ MyLogo } alt="loader_dog" className='loader_dog'></img>
       </div>
     </div>
   </div>
