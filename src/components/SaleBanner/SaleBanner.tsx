0f07f1ff6f4f95f72b7a68de2669721ff143e226
Bogdan <bodia-00wot@ukr.net>
2023-05-09 15:57:32 +0300

    added files

---
+++ src/components/SaleBanner/SaleBanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import saleBanner from './SaleBanner.module.scss';
+
+export const SaleBanner: React.FC = () => {
+
+  return (
+    <div className= { saleBanner.saleBanner}>
+      
+    </div>
+  );
+};
