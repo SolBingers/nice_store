b8f87c058906ee42983515038a2cb1de409242bd
NikitaStambul <stambul.n@gmail.com>
2023-05-09 22:24:59 +0300

    header done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
 import header from './Header.module.scss';
-import { SearchBar } from '../SearchBar';
 import { ReactComponent as Burger } from '../../images/burger.svg';
 import { ReactComponent as Favourites } from '../../images/favourites.svg';
 import { ReactComponent as Cart } from '../../images/cart.svg';
@@ -27,23 +26,7 @@ export const Header: FC = () => {
           </button>
         </div>
 
-        <div className={classNames(
-          header.section, 
-          header.searchBarSection,
-          {
-            [header.searchBarSection_showed] : isModalSearchShowed,
-          })}
-        >
-          <SearchBar />
-        </div>
-
         <div className={header.section}>
-          <button 
-            className={classNames(header.button, header.lensButton)}
-            onClick={handleLenseClick}
-          >
-            <Lens className={classNames(header.lens, header.icon)} />
-          </button>
           <button className={header.button}>
             <Favourites className={classNames(header.favourites, header.icon)} />
           </button>
