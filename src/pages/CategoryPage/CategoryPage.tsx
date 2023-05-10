95f689ca375c1b3f5712e7d2a4cca5ca561b69f2
NikitaStambul <stambul.n@gmail.com>
2023-05-10 22:30:55 +0300

    start category page

--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -1,17 +1,35 @@
 import React, { FC } from 'react';
 import { useParams } from 'react-router-dom';
-import { Categories } from '../../components/Categories';
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+import styles from './CategoryPage.module.scss';
+import { SettingsInput } from '../../components/SettingsInput';
+import { SettingsSelect } from '../../components/SettingsSelect';
 
 export const CategoryPage: FC = () => {
   const { selectedCategory } = useParams();
 
   return (
-    <>
-      <h1>
-        Category Page {selectedCategory}
-      </h1>
-      <Categories />
-    </>
+    <div className={styles.main}>
+      <Header className={styles.header} />
 
+      <div className={styles.content}>
+        <p className={styles.title}>
+          {selectedCategory}
+        </p>
+
+        <div className={styles.settings}>
+          <SettingsInput title="Product name" />
+          <SettingsSelect 
+            title="Sort by"
+            options={['Newest', 'Oldest']}
+            selected = 'Newest'
+            setSelected={() => {return;}}
+          />
+        </div>
+      </div>
+
+      <Footer className={styles.footer} />
+    </div>
   );
 };
