277aa8493bf462609cb37c4ba7c7e6c133131aff
Bogdan <bodia-00wot@ukr.net>
2023-05-10 16:46:15 +0300

    added files

---
+++ src/components/List/List.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import listStyle from './List.module.scss';
+
+export const List: React.FC = () => {
+  return (
+    <div className={listStyle.container}>
+
+    </div>
+  );
+};
