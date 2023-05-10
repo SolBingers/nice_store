256a17c2ff42c92f13333aa6b8545f46d36834e9
NikitaStambul <stambul.n@gmail.com>
2023-05-10 15:13:04 +0300

    hover + focus logic added

--- src/components/SettingsInput/SettingsInput.tsx
+++ src/components/SettingsInput/SettingsInput.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react';
 import settingsInput from './SettingsInput.module.scss';
+import classNames from 'classnames';
 
 type Props = {
   className?: string;
@@ -8,7 +9,7 @@ type Props = {
 
 export const SettingsInput: FC<Props> = ({ className, title }) => {
   return (
-    <div className={className} >
+    <div className={classNames(className, settingsInput.main)} >
       <p className={settingsInput.title} >{title}</p>
       <input 
         type="text" 
