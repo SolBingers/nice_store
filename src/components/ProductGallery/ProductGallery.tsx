65198d100e00d8daee6480a4299b9e74f86ad636
Kateryna Babina <katerynababina01@gmail.com>
2023-05-10 23:12:02 +0300

    created image gallery

---
+++ src/components/ProductGallery/ProductGallery.tsx
@@ -0,0 +1,47 @@
+import classNames from 'classnames';
+import React, { FC, useState } from 'react';
+import gallery from './ProductGallery.module.scss';
+
+type ProductGalleryProps = {
+  images: string[];
+};
+
+export const ProductGallery: FC<ProductGalleryProps> = ({ images }) => {
+  const [mainPhoto, setMainPhoto] = useState(images[0]);
+
+  return (
+    <div className={gallery.gallery}>
+      <div className={gallery.mainPhotoContainer}>
+        <img
+          src={mainPhoto}
+          alt="phone__image"
+          className={gallery.mainImage}
+        />
+      </div>
+
+      <div className={gallery.images}>
+        {images.map((image) => {
+          const imageId = image.split('/').reverse()[0];
+
+          return (
+            <div
+              key={imageId}
+              className={classNames([gallery.imagesContainer], {
+                [gallery.isActive]: image === mainPhoto,
+              }
+              )}
+            >
+              <img
+                key={imageId}
+                src={image}
+                alt="phone__image"
+                className={gallery.image}
+                onClick={() => setMainPhoto(image)}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
