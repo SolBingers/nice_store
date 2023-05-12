a5466be2f8c4368fa9eaed578fbc7407697f98ad
Anastasiia Kuznetsova <an.stsii.k@gmail.com>
2023-05-12 11:11:28 +0200

    added query params to fetch client

--- src/api/phones.ts
+++ src/api/phones.ts
@@ -1,9 +1,32 @@
 import { Phone, PhoneSpec } from '../components/types/types';
 import { client } from '../utils/fetchClient';
 
+interface Params {
+  page?: number,
+  count?: number,
+  sort?: 'newest' | 'oldest',
+}
 
-export const getAllPhones = async (): Promise<Phone[]> =>
-  client.get('/products');
+export const getAllPhones = async ({
+  page,
+  count,
+  sort,
+}: Params): Promise<Phone[]> => {
+  const params = [];
+  if (page) {
+    params.push(`page=${page}`);
+  }
+  if (count) {
+    params.push(`count=${count}`);
+  }
+  if (sort) {
+    params.push(`sort=${sort}`);
+  }
+
+  const query = params ? params.join('&') : '';
+
+  return client.get(`/products?${query}`);
+};
 
 export const getPhoneById = (id: string): Promise<PhoneSpec> =>
   client.get(`/products/${id}`);
--- src/pages/CategoryPage/CategoryPage.tsx
+++ src/pages/CategoryPage/CategoryPage.tsx
@@ -7,7 +7,7 @@ import styles from './CategoryPage.module.scss';
 import { List } from '../../components/List';
 import { Categories } from '../../components/Categories';
 import { Pagination } from '../../components/Pagination';
-import { getPhonesByType } from '../../api/phones';
+import { getAllPhones, getPhonesByType } from '../../api/phones';
 import { Phone } from '../../components/types/types';
 
 type Props = {
