06e62acef33c1c579e9b178b45847752f2923fe2
Bogdan <bodia-00wot@ukr.net>
2023-05-12 23:09:37 +0300

    solution

---
+++ src/components/CartCounter/CartCounter.tsx
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import counterStyle from './CartCounter.module.scss';
+import { CartContext } from '../../contexts/CartContext';
+
+export const CartCounter: React.FC = () => {
+  const { cart } = useContext(CartContext);
+
+  let count = 0;
+
+  cart.forEach(product => {
+    count += product.count;
+  });
+
+  return (
+    <div className={counterStyle.container}>
+      {`${count}`}
+    </div>
+  );
+};
