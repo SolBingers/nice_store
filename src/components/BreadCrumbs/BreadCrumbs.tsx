8fa7efed22ccfe71544cc932e388b6b331241ffd
Kateryna Babina <katerynababina01@gmail.com>
2023-05-13 12:00:58 +0300

    changed codestyle

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,39 +1,32 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import styles from './BreadCrumbs.module.scss';
-import classNames from 'classnames';
+import breadcrumbs from './BreadCrumbs.module.scss';
 
-export const BreadCrumbs: React.FC = () => {
+export const BreadCrumbs = () => {
   const location = useLocation();
-  const paths = location.pathname.split('/').filter((x) => x);
+
+  const paths = useMemo(() => {
+    const segments = location.pathname.split('/').filter(segment => segment !== '');
+    const paths = segments.map((segment, index) => ({
+      name: segment.charAt(0).toUpperCase() + segment.slice(1).replace(/-/g, ' '),
+      path: `/${segments.slice(0, index + 1).join('/')}`
+    }));
+    return paths;
+  }, [location.pathname]);
 
   return (
-    <nav aria-label={styles.breadcrumbs}>
-      <ul className={styles.breadcrumbs}>
-        <li>
-          <Link to='/'> 🤝 </Link>
-        </li>
-        {paths.map((path, i) => {
-          const to = `/${paths.slice(0, i + 1).join('/')}`;
-          const isActive = to === location.pathname;
-          const pathName = path[0].toUpperCase() + path.slice(1);
-
-          return (
-            <li
-              className={classNames([styles.item], { [styles.active]: isActive })}
-              aria-current={isActive ? 'page' : undefined}
-              key={to}
-            >
-              {isActive ? (
-                pathName
-              ) : (
-                <Link to={to}>{pathName}</Link>
-              )}
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
+    <div className={breadcrumbs.breadcrumbs}>
+      <Link to="/" className={breadcrumbs.homeLink}>
+        🤝
+      </Link>
+      {paths.map((path) => (
+        <div key={path.path}>
+          <div className={breadcrumbs.arrow}></div>
+          <Link to={path.path} className={breadcrumbs.pathLink}>
+            {path.name}
+          </Link>
+        </div>
+      ))}
+    </div>
   );
 };
-
