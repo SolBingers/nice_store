f5b74dcda6c0b46af1f485e4aa23545dc05a9bc3
Kateryna Babina <katerynababina01@gmail.com>
2023-05-13 14:48:42 +0300

    breadcrumbs done and added on pages

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,16 +1,18 @@
 import React, { useMemo } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import breadcrumbs from './BreadCrumbs.module.scss';
+import classNames from 'classnames';
 
 export const BreadCrumbs = () => {
   const location = useLocation();
 
   const paths = useMemo(() => {
     const segments = location.pathname.split('/').filter(segment => segment !== '');
-    const paths = segments.map((segment, index) => ({
+    const paths = segments.map((segment, i) => ({
       name: segment.charAt(0).toUpperCase() + segment.slice(1).replace(/-/g, ' '),
-      path: `/${segments.slice(0, index + 1).join('/')}`
+      path: `/${segments.slice(0, i + 1).join('/')}`
     }));
+
     return paths;
   }, [location.pathname]);
 
@@ -19,10 +21,14 @@ export const BreadCrumbs = () => {
       <Link to="/" className={breadcrumbs.homeLink}>
         🤝
       </Link>
-      {paths.map((path) => (
-        <div key={path.path}>
-          <div className={breadcrumbs.arrow}></div>
-          <Link to={path.path} className={breadcrumbs.pathLink}>
+
+      {paths.map((path, i) => (
+        <div className={breadcrumbs.crumb} key={path.path}>
+          <span className={breadcrumbs.arrow}>&gt;</span>
+
+          <Link to={path.path} className={classNames(breadcrumbs.pathLink, {
+            [breadcrumbs.active]: i === paths.length - 1,
+          })}>
             {path.name}
           </Link>
         </div>
