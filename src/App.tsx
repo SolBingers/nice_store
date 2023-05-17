f839bcb2197db5a44482ac1b88382f1860e32ed9
Bogdan <bodia-00wot@ukr.net>
2023-05-17 20:11:06 +0300

    fixed colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import './styles/main.scss';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage';
