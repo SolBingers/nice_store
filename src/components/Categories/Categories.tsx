ec93236ede27a92cd129e1838b8a6383cbe15c46
NikitaStambul <stambul.n@gmail.com>
2023-05-17 12:32:11 +0300

    categories chevron and animation fix

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react';
 import styles from './Categories.module.scss';
 import { NavLink, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
+import { ReactComponent as Arrow } from '../../images/chevron-down.svg';
 
 type Props = {
   className?: string,
@@ -11,7 +12,6 @@ export const Categories: FC<Props> = ({ className }) => {
   const [isClosed,setIsClosed] = useState(false);
   const { search } = useLocation();
 
-  
   return (
     <div className={classNames(
       styles.categories,
@@ -22,7 +22,7 @@ export const Categories: FC<Props> = ({ className }) => {
           Categories
         </div>
 
-        <button 
+        <Arrow 
           onClick={() => setIsClosed(!isClosed)}
           className={classNames(
             styles.categories__input, 
@@ -32,61 +32,64 @@ export const Categories: FC<Props> = ({ className }) => {
           )} 
         />
       </div>
-      {!isClosed && (
-        <nav className={styles.categories__dropdown}>
-          <div className={styles.categories__container}>
+      <nav className={classNames(
+        styles.categories__dropdown,
+        {
+          [styles.invisible]: isClosed,
+        }
+      )}>
+        <div className={styles.categories__container}>
 
-            <NavLink 
-              className={({ isActive }) => classNames(
-                styles.categories__customLink,
-                {
-                  [styles.activeLink]: isActive,
-                },
-              )} 
-              to={{
-                pathname: '/phones',
-                search,
-              }}
-            >
-              Phones
-            </NavLink> 
+          <NavLink 
+            className={({ isActive }) => classNames(
+              styles.categories__customLink,
+              {
+                [styles.activeLink]: isActive,
+              },
+            )} 
+            to={{
+              pathname: '/phones',
+              search,
+            }}
+          >
+            Phones
+          </NavLink> 
 
-            <NavLink
-              className={({ isActive }) => classNames(
-                styles.categories__customLink,
-                {
-                  [styles.activeLink]: isActive,
-                },
-              )}  
-              to={{
-                pathname: '/tablets',
-                search,
-              }}
-            >
-              Tablets
-            </NavLink>  
+          <NavLink
+            className={({ isActive }) => classNames(
+              styles.categories__customLink,
+              {
+                [styles.activeLink]: isActive,
+              },
+            )}  
+            to={{
+              pathname: '/tablets',
+              search,
+            }}
+          >
+            Tablets
+          </NavLink>  
 
-            <NavLink
-              className={({ isActive }) => classNames(
-                styles.categories__customLink,
-                {
-                  [styles.activeLink]: isActive,
-                },
-              )} 
-              to={{
-                pathname: '/accessories',
-                search,
-              }}
-            >
-              Accessories
-            </NavLink>
-          </div>
+          <NavLink
+            className={({ isActive }) => classNames(
+              styles.categories__customLink,
+              {
+                [styles.activeLink]: isActive,
+              },
+            )} 
+            to={{
+              pathname: '/accessories',
+              search,
+            }}
+          >
+            Accessories
+          </NavLink>
+        </div>
 
-          <div className={styles.categories__rights}>
-            All rights reserved.
-          </div>
-        </nav>
-      )}
+        <div className={styles.categories__rights}>
+          All rights reserved.
+        </div>
+      </nav>
     </div>
   );
 };
